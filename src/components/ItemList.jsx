a8bd2e59c66a734259b65fc4721a0c2ca2e1a5a7
Juan_manuel <jaredjuan71@gmail.com>
2024-06-21 16:10:53 -0400

    tercer

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const ItemList = ({children}) => {
   return (
-    <ul className='grid gap-2 grid-cols-3 grid-rows-3  m-2 '>
+    <ul className='grid gap-2 grid-cols-2 grid-rows-3  m-2 '>
       {children}
     </ul>
   )
